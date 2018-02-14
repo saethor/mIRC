f123c97e5c026e3e6a3a57e8c6214b17c9ce4779
Sæþór Hallgrímsson <saethor@hringdu.is>
2018-02-14 14:40:55 +0000

    Added join room method

--- src/components/JoinRoom/JoinRoom.spec.js
+++ src/components/JoinRoom/JoinRoom.spec.js
@@ -8,17 +8,25 @@ import JoinRoom from './JoinRoom';
 jest.useFakeTimers();
 
 describe('JoinRoom test', () => {
-    let mockSocketServer, mockSocket, error, log;
+    let mockSocketServer, mockSocket, stub;
 
     beforeEach(() => {
-        error = sinon.stub(console, 'error');
-        log = sinon.stub(console, 'log');
+        stub = sinon.stub(console, 'error');
         mockSocketServer = new Server('http://localhost:3050');
         mockSocketServer.on('connection', socket =>  {
-            socket.on('joinRoom', (joinObj, fn) => {
+            socket.on('joinroom', (joinObj, fn) => {
                 var room = joinObj.room;
                 var pass = joinObj.pass;
                 var accepted = true;
+                if (pass == 'hunter2') {
+                    fn(true);
+                }
+                else if (pass == 'banned') {
+                    fn(false, 'banned');
+                }
+                else {
+                    fn(false, 'wrong password');
+                }
             });
         });
 
@@ -28,23 +36,52 @@ describe('JoinRoom test', () => {
     });
 
     it('should render the title of the room', () => {
-        const component = shallow(<JoinRoom name="General" onSubmit={() => console.log(true)} />);
+        const component = shallow(<JoinRoom name="General" />);
         expect(component.find('h3').first().text()).toEqual('General');
     })
 
     it('should render correctly when receiving correct prop', () => {
-        const component = shallow(<JoinRoom name="" onSubmit={() => console.log()} />);
-        expect(error.notCalled).toBe(true);
+        const component = shallow(<JoinRoom name="" />);
+        expect(stub.notCalled).toBe(true);
     });
 
     it('should not render correctly when not receiving all required props', () =>  {
-        const component = shallow(<JoinRoom name="" />);
-        expect(error.called).toBe(true);
+        const component = shallow(<JoinRoom  />, { context: { socket: mockSocket } } );
+        expect(stub.called).toBe(true);
+    });
+
+    it('should emit true when joining room', () => {
+        const password = "hunter2", room = "asdf";
+        const component = shallow(<JoinRoom name={room} />, { context: { socket: mockSocket } } );
+
+        component.instance().join(password);
+
+        expect(component.state().room).toBe(room);
+        expect(component.state().feedback).toBe(undefined);
+    });
+
+    it('should provide reason when failing to join room', () => {
+        const password = "password", room = "asdf";
+        const component = shallow(<JoinRoom name={room} />, { context: { socket: mockSocket } });
+
+        component.instance().join(password);
+
+        expect(component.state().room).toBe(room);
+        expect(component.state().feedback).toBe("wrong password");
+    });
+
+    it('should return banned when user is banned', () => {
+        const password = "banned", room = "asdf";
+        const component = shallow(<JoinRoom name={room} />, { context: { socket: mockSocket } });
+
+        component.instance().join(password);
+
+        expect(component.state().room).toBe(room);
+        expect(component.state().feedback).toBe("banned");
     })
 
     afterEach(() => {
         console.error.restore();
-        console.log.restore();        
         mockSocketServer.stop();
         mockSocket.close();
     });
