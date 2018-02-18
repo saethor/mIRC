9cf8df03e1cb1cda87d1c79bc0650031ed195437
Sæþór Hallgrímsson <saethor94@gmail.com>
2018-02-18 15:05:02 +0000

    Changed rooms to be link

--- src/components/RoomItem/RoomItem.js
+++ src/components/RoomItem/RoomItem.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 
-const RoomItem = ({ name, locked, onClick }) => (
-    <ul onClick={() => onClick(name)}>
-        <h3>{name}</h3>
-        <i>{locked}</i>
-    </ul>
+const RoomItem = ({ name }) => (
+    <li onClick={() => onClick(name)}>
+        <Link to={`/rooms/${name}`}>{name}</Link>
+    </li>
 );
-
-RoomItem.defaultProps = {
-    locked: false
-}
 
 RoomItem.propTypes = {
     name: PropTypes.string.isRequired,
-    locked: PropTypes.bool,
-    onClick: PropTypes.func.isRequired
 }
 
 export default RoomItem;
--- src/components/RoomItem/RoomItem.spec.js
+++ src/components/RoomItem/RoomItem.spec.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import sinon from 'sinon';
 import { shallow } from 'enzyme';
 
 import RoomItem from './RoomItem'
 
 describe('RoomItem tests', () => {
-    let stub;
-
-    beforeEach(() => {
-        stub = sinon.stub(console, 'error');
-    });
-
     it('should render room name correctly', () => {
         const name = 'General';
         const component = shallow(<RoomItem name={name} onClick={() => console.error(name)} />)
-        expect(component.find('h3').first().text()).toBe(name);
-    });
-
-    it('should trigger onClick on click', () => {
-        const name = 'General';
-        const component = shallow(<RoomItem name={name} onClick={() => console.error(name)} />)
-
-        component.find('ul').first().simulate('click');
-        expect(stub.calledOnce).toBe(true);
-    })
-
-    afterEach(() => {
-        console.error.restore()
+        expect(component.find('li').first().text()).toBe('<Link />');
     });
 });
