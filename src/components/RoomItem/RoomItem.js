3584fd8b5525a9d18bd7f9c6cb999b8bd403c0c6
Sæþór Hallgrímsson <saethor94@gmail.com>
2018-02-18 15:27:54 +0000

    Forgot to take out onClick()

--- src/components/RoomItem/RoomItem.js
+++ src/components/RoomItem/RoomItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
 
 const RoomItem = ({ name }) => (
-    <li onClick={() => onClick(name)}>
+    <li>
         <Link to={`/rooms/${name}`}>{name}</Link>
     </li>
 );
