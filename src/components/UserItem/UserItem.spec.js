e012881e54a656452f45642acec490e2f8cd5b5e
Sæþór Hallgrímsson <saethor@hringdu.is>
2018-02-19 12:37:58 +0000

    fixed failing test

--- src/components/UserItem/UserItem.spec.js
+++ src/components/UserItem/UserItem.spec.js
@@ -2,16 +2,55 @@ import React from 'react';
 import UserItem from './UserItem.js';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
+import { SocketIO, Server } from 'mock-socket';
 
-describe('UserItem tests', () => {
-    let stub;
-    
+jest.useFakeTimers();
+
+describe('UserItem tests', () => {    
+    const user = "foo";
+    let mockSocketServer, mockSocket, stub;
+
     beforeEach(() => {
         stub = sinon.stub(console, 'error');
+        mockSocketServer = new Server('http://localhost:3050');
+        mockSocketServer.on('connection', socket =>  {
+            socket.on('op', (opObj, fn) => {
+                if (opObj.user === user) {
+                    fn(true);
+                } else {
+                    fn(false)
+                }
+            });
+            socket.on('deop', (deopObj, fn) => {
+                if (opObj.user === user) {
+                    fn(true);
+                } else {
+                    fn(false)
+                }
+            });
+            socket.on('ban', (banObj, fn) => {
+                if (opObj.user === user) {
+                    fn(true);
+                } else {
+                    fn(false)
+                }
+            });
+            socket.on('kick', (kickObj, fn) => {
+                if (opObj.user === user) {
+                    fn(true);
+                } else {
+                    fn(false)
+                }
+            });
+        });
+
+        mockSocket = SocketIO.connect('http://localhost:3050');
+
+        jest.runOnlyPendingTimers();
     });
 
     it('should not render without required name property', () => {
-        shallow(<UserItem />);
+        shallow(<UserItem />, { context: { socket: mockSocket}});
 
         expect(stub.calledOnce).toBe(true);
     });
@@ -20,7 +59,7 @@ describe('UserItem tests', () => {
         const name = 'me';
         const component = shallow(<UserItem name={name} />);
 
-        let text = component.find('li').first().text();
+        let text = component.find('li').first().find('span').first().text();
 
         expect(text).toEqual(name);
     });
@@ -29,12 +68,14 @@ describe('UserItem tests', () => {
         const name = 'me';
         const component = shallow(<UserItem name={name} admin={true} />);
     
-        let text = component.find('li').first().text();
+        let text = component.find('li').first().find('span').first().text();
     
         expect(text).toEqual(`@${name}`);
     });
 
     afterEach(() => {
         console.error.restore();
+        mockSocketServer.stop();
+        mockSocket.close();
     });
 });
